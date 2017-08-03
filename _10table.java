d83a58cad230c0dc7f5d8bbcc6d66b974597410f
srikanth <7srikanth7@gmail.com>
2017-08-03 15:31:35 -0400

    testing 10 table

---
+++ _10table.java
@@ -0,0 +1,17 @@
+package forloop;
+
+public class _10table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ten table ");
+		System.out.println("Ten table ");
+		
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("10 * " + i +" = " + (10*i));
+		}
+
+	}
+
+}
